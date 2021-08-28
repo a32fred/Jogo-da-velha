129880ab96c95ed11b2118ebee18e35b6b7f311c
a32fred <a.32.freeed@gmail.com>
2021-08-28 19:29:53 -0300

    paciencia

---
+++ script/jogo.js
@@ -0,0 +1,21 @@
+
+(()=>{ 
+
+//declarar variáveis que será usada no jogo;
+var tabuleiro = [
+    [0 = document.getElementById("casa1").value, 1 = document.getElementById("casa2").value, 2 = document.getElementById("casa3").value],
+    [3 = document.getElementById("casa4").value, 4 = document.getElementById("casa5").value, 5 = document.getElementById("casa6").value],
+    [6 = document.getElementById("casa7").value, 7 = document.getElementById("casa8").value, 8 = document.getElementById("casa9").value]];
+
+var gameover = true;
+
+//captura o click e adciona a imagem nos espaços
+function click(){
+    console.log("funciona pelo amor de deus");
+}
+
+
+
+
+    
+})()
